7fc3067ae78441809355c08f434e83121538a584
Michal Lutecki <michal.lutecki.be@gmail.com>
2024-10-24 20:14:39 +0200

    update

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,6 +1,6 @@
 export const VARIABLES = {
-  AVATAR_URL: '/405654518692776202.vrm',
+  AVATAR_URL: '/2367421739220731668.vrm',
   OPENAI_API_KEY: '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',
   ELEVEN_LABS_API_KEY: 'sk_ae64b5bbed1b9908d396af7caa2467330ee8b985f1330c3f',
-  name: 'TruthlullyAI'
+  name: 'Diana'
 }
